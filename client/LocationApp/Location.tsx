aca1ce46a541768388ceeb0d9725d608d86c30a5
Andrew Thresher <thresh3@me.com>
2023-03-06 21:33:21 -0600

    Add refresh and new button

--- client/LocationApp/Location.tsx
+++ client/LocationApp/Location.tsx
@@ -2,7 +2,7 @@ import { Locations } from '@shared/interfaces/locations';
 import { LocationData } from '@shared/types/location';
 import axios from 'axios';
 import React, { useContext, useEffect, useState } from 'react';
-import { Section, Table } from 'react-bulma-components';
+import { Button, Level, Section, Table } from 'react-bulma-components';
 import ToastContext from '../common/contexts/toast';
 
 const LocationApp: React.FC = () => {
@@ -10,16 +10,40 @@ const LocationApp: React.FC = () => {
   const [loading, setLoading] = useState<boolean>(true);
   const toastContext = useContext(ToastContext);
 
-  useEffect(() => {
+  const fetch = () => {
     axios
       .get<Locations.Response>('/api/locations')
       .then((response) => setLocations(response.data))
       .catch(() => toastContext.addToast({ message: `Cannot get Locations`, color: 'danger' }))
       .finally(() => setLoading(false));
+  };
+
+  useEffect(() => {
+    fetch();
   }, []);
+
+  const refresh = () => {
+    if (loading) {
+      return;
+    }
+    setLoading(true);
+    fetch();
+  };
 
   return (
     <Section>
+      <Level>
+        <Level.Side>
+          <Level.Item>
+            <Button>New Location</Button>
+          </Level.Item>
+        </Level.Side>
+        <Level.Side>
+          <Level.Item>
+            <Button onClick={refresh}>Refresh</Button>
+          </Level.Item>
+        </Level.Side>
+      </Level>
       <Table size='fullwidth' hoverable striped>
         <thead>
           <tr>
