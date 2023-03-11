19d3bb4508ece3ac24435c320a67503572b46283
Andrew Thresher <thresh3@me.com>
2023-03-11 00:24:23 -0600

    Use URL to determine Details subpage

--- client/LocationApp/Details/Details.tsx
+++ client/LocationApp/Details/Details.tsx
@@ -5,17 +5,18 @@ import { LocationData } from '@shared/types/location';
 import axios from 'axios';
 import React, { useContext, useEffect, useState } from 'react';
 import { Columns, Heading, Icon, Menu, Section } from 'react-bulma-components';
-import { Outlet, useNavigate, useParams } from 'react-router-dom';
+import { Outlet, useMatch, useNavigate, useParams } from 'react-router-dom';
 import ToastContext from '../../common/contexts/toast';
 
-type LocationDetailsTab = 'dashboard' | 'contacts' | 'units' | 'debt' | 'cashflow' | 'projections';
-
 const LocationDetails = () => {
-  const { locationId } = useParams();
   const navigate = useNavigate();
+  const { locationId } = useParams();
+  const match = useMatch('/locations/:locationId/:page');
+  const page = match?.params.page ?? 'dashboard';
+  const detailsUrl = `/locations/${locationId}`;
+
   const [location, setLocation] = useState<LocationData | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
-  const [tab, setTab] = useState<LocationDetailsTab>('dashboard');
   const { addToast } = useContext(ToastContext);
 
   useEffect(() => {
@@ -42,26 +43,44 @@ const LocationDetails = () => {
         <Columns.Column size='one-fifth'>
           <Menu>
             <Menu.List title='General'>
-              <Menu.List.Item active={tab === 'dashboard'} onClick={() => setTab('dashboard')}>
+              <Menu.List.Item
+                active={page === 'dashboard'}
+                onClick={() => navigate(`${detailsUrl}`)}
+              >
                 Dashboard
               </Menu.List.Item>
-              <Menu.List.Item active={tab === 'contacts'} onClick={() => setTab('contacts')}>
+              <Menu.List.Item
+                active={page === 'contacts'}
+                onClick={() => navigate(`${detailsUrl}/contacts`)}
+              >
                 Contacts
               </Menu.List.Item>
             </Menu.List>
             <Menu.List title='Management'>
-              <Menu.List.Item active={tab === 'units'} onClick={() => setTab('units')}>
+              <Menu.List.Item
+                active={page === 'units'}
+                onClick={() => navigate(`${detailsUrl}/units`)}
+              >
                 Units
               </Menu.List.Item>
             </Menu.List>
             <Menu.List title='Finance'>
-              <Menu.List.Item active={tab === 'debt'} onClick={() => setTab('debt')}>
+              <Menu.List.Item
+                active={page === 'debt'}
+                onClick={() => navigate(`${detailsUrl}/debt`)}
+              >
                 Debt
               </Menu.List.Item>
-              <Menu.List.Item active={tab === 'cashflow'} onClick={() => setTab('cashflow')}>
+              <Menu.List.Item
+                active={page === 'cashflow'}
+                onClick={() => navigate(`${detailsUrl}/cashflow`)}
+              >
                 Cashflow
               </Menu.List.Item>
-              <Menu.List.Item active={tab === 'projections'} onClick={() => setTab('projections')}>
+              <Menu.List.Item
+                active={page === 'projections'}
+                onClick={() => navigate(`${detailsUrl}/projections`)}
+              >
                 Projections
               </Menu.List.Item>
             </Menu.List>
