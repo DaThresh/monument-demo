0d73da02d7b53208d1c0fa8a651ac215a56d57df
Andrew Thresher <thresh3@me.com>
2023-03-10 23:17:40 -0600

    Add Form Components

---
+++ client/common/components/Forms.tsx
@@ -0,0 +1,52 @@
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import { Columns, Form, Icon } from 'react-bulma-components';
+
+export const TextInput: React.FC<{
+  name: string;
+  label?: string;
+  type?: React.HTMLInputTypeAttribute;
+  value: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  required?: boolean;
+  iconLeft?: IconProp;
+  columns?: boolean;
+}> = ({
+  name,
+  label,
+  type = 'text',
+  value,
+  onChange,
+  required = false,
+  iconLeft,
+  columns = false,
+}) => {
+  const formLabel = <Form.Label>{label ?? name}</Form.Label>;
+  const formControl = (
+    <Form.Control>
+      {iconLeft && (
+        <Icon align='left' size='small'>
+          <FontAwesomeIcon icon={iconLeft} />
+        </Icon>
+      )}
+      <Form.Input type={type} name={name} value={value} onChange={onChange} required={required} />
+    </Form.Control>
+  );
+
+  return (
+    <Form.Field>
+      {columns ? (
+        <Columns vCentered>
+          <Columns.Column size='one-fifth'>{formLabel}</Columns.Column>
+          <Columns.Column>{formControl}</Columns.Column>
+        </Columns>
+      ) : (
+        <>
+          {formLabel}
+          {formControl}
+        </>
+      )}
+    </Form.Field>
+  );
+};
