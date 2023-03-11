5025523d7f626967406d719ee6c7d7175568e706
Andrew Thresher <thresh3@me.com>
2023-03-10 23:18:46 -0600

    Add Modal, Location listing

--- client/App.tsx
+++ client/App.tsx
@@ -1,9 +1,11 @@
 import React, { Suspense } from 'react';
 import { hot } from 'react-hot-loader/root';
 import { Route, Routes } from 'react-router-dom';
+import Modal from './common/components/Modal';
 import Toasts from './common/components/Toasts';
 
 const LocationApp = React.lazy(() => import('./LocationApp/Location'));
+const LocationDetails = React.lazy(() => import('./LocationApp/Details'));
 const LandingApp = React.lazy(() => import('./LandingApp/Landing'));
 
 const App: React.FC = () => {
@@ -11,11 +13,13 @@ const App: React.FC = () => {
     <>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
+          <Route path='/locations/:locationId' element={<LocationDetails />} />
           <Route path='/locations' element={<LocationApp />} />
           <Route index element={<LandingApp />} />
         </Routes>
       </Suspense>
 
+      <Modal />
       <Toasts />
     </>
   );
--- client/LocationApp/Location.tsx
+++ client/LocationApp/Location.tsx
@@ -3,39 +3,59 @@ import { LocationData } from '@shared/types/location';
 import axios from 'axios';
 import React, { useContext, useEffect, useState } from 'react';
 import { Button, Level, Section, Table } from 'react-bulma-components';
+import { useNavigate } from 'react-router-dom';
+import ModalContext from '../common/contexts/modal';
 import ToastContext from '../common/contexts/toast';
+import NewLocationModal from './modals/NewLocation';
 
 const LocationApp: React.FC = () => {
   const [locations, setLocations] = useState<LocationData[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
-  const toastContext = useContext(ToastContext);
+  const navigate = useNavigate();
+  const { addToast } = useContext(ToastContext);
+  const { openModal } = useContext(ModalContext);
 
   const fetch = () => {
     axios
       .get<Locations.Response>('/api/locations')
       .then((response) => setLocations(response.data))
-      .catch(() => toastContext.addToast({ message: `Cannot get Locations`, color: 'danger' }))
+      .catch(() => addToast({ message: `Cannot get Locations`, color: 'danger' }))
       .finally(() => setLoading(false));
   };
-
-  useEffect(() => {
-    fetch();
-  }, []);
 
   const refresh = () => {
     if (loading) {
       return;
     }
     setLoading(true);
+    setLocations([]);
     fetch();
   };
+
+  const openLocation = (locationId: number) => {
+    navigate(`/locations/${locationId}`);
+  };
+
+  const openNewLocation = () => {
+    const addLocation = (location: LocationData) => {
+      setLocations((locations) => [...locations, location]);
+    };
+
+    openModal(<NewLocationModal addLocation={addLocation} />);
+  };
+
+  useEffect(() => {
+    fetch();
+  }, []);
 
   return (
     <Section>
       <Level>
         <Level.Side>
           <Level.Item>
-            <Button>New Location</Button>
+            <Button color='info' onClick={openNewLocation}>
+              New Location
+            </Button>
           </Level.Item>
         </Level.Side>
         <Level.Side>
@@ -55,7 +75,11 @@ const LocationApp: React.FC = () => {
         </thead>
         <tbody>
           {locations.map((location) => (
-            <tr key={location.id}>
+            <tr
+              key={location.id}
+              onClick={() => openLocation(location.id)}
+              style={{ cursor: 'pointer' }}
+            >
               <td>{location.name}</td>
               <td>{location.address}</td>
               <td>
