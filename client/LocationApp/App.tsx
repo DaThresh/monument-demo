50c5171048dcdf0f47f8238d0629c214f6f6f663
Andrew Thresher <thresh3@me.com>
2023-03-11 00:02:04 -0600

    Move details tabs to new directory

--- client/LocationApp/App.tsx
+++ client/LocationApp/App.tsx
@@ -1,7 +1,7 @@
 import React, { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import DetailsDashboard from './Details/Dashboard';
-import UnitManagement from './Details/UnitManagement';
+import DetailsDashboard from './Details/Tabs/Dashboard';
+import UnitManagement from './Details/Tabs/UnitManagement';
 
 const LocationList = React.lazy(() => import('./List/LocationList'));
 const LocationDetails = React.lazy(() => import('./Details/Details'));
